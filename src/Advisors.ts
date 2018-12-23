d4a452ffebf93c3df9c86663a50c5460caab538e
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-12-24 00:02:39 +0100

    Use union types for Advisors where possible

--- src/Advisors.ts
+++ src/Advisors.ts
@@ -1,64 +1,51 @@
 /**
+ * @example
  * ```
  * {
- *   name: "Sesostris_L_IV",
+ *   name: 'Sesostris_L_IV',
  *   groupid: 121,
  *   age: 3,
- *   rarity: "legendary",
- *   icon: "Celeste\\UserInterface\\Icons\\Advisors\\AdvisorSesostris5_db",
- *   icontexturecoords: "0 0 1 1.0",
- *   rollovertextid: "",
+ *   rarity: 'legendary',
+ *   icon: 'Celeste\UserInterface\Icons\Advisors\AdvisorSesostris5_db',
+ *   icontexturecoords: '0 0 1 1.0',
+ *   rollovertextid: '',
  *   displaynameid: 150090,
  *   displaydescriptionid: 150091,
- *   sellcostoverride: {
- *     capitalresource: {
- *       quantity: 900,
- *       type: "cCapResCoin",
- *     },
- *   },
+ *   sellcostoverride: { capitalresource: { quantity: 900, type: 'cCapResCoin' } },
  *   minlevel: 40,
  *   itemlevel: 40,
- *   offertype: "eOfferCeleste1",
- *   techs: {
- *     tech: "Advisor_Sesostris_L",
- *   },
- *   sellable: "true",
- *   tradeable: "true",
- *   destroyable: "true",
- *   civilization: "Egypt",
- *   shortdescriptionid: "61615",
- *   specialborder: "true",
+ *   offertype: 'eOfferCeleste1',
+ *   techs: { tech: 'Advisor_Sesostris_L' },
+ *   sellable: 'true',
+ *   tradeable: 'true',
+ *   destroyable: 'true',
+ *   civilization: 'Egypt',
+ *   shortdescriptionid: '61615',
+ *   specialborder: 'true',
  * }
  * ```
  */
 export interface Advisor {
   name: string
   groupid: number
-  age: number
-  rarity: string
+  age: 0 | 1 | 2 | 3
+  rarity: "common" | "uncommon" | "rare" | "epic" | "legendary"
   icon: string
-  icontexturecoords: string
-  rollovertextid: string
+  icontexturecoords: "0 0 1 1.0"
+  rollovertextid: ""
   displaynameid: number
   displaydescriptionid: number
-  sellcostoverride: {
-    capitalresource: {
-      quantity: number
-      type: string,
-    },
-  }
-  minlevel: number
-  itemlevel: number
-  offertype: string
-  techs: {
-    tech: string,
-  }
-  sellable: string // FIXME should be `boolean`
-  tradeable: string // FIXME should be `boolean`
-  destroyable: string // FIXME should be `boolean`
-  civilization?: string
+  sellcostoverride: { capitalresource: { quantity: number, type: string } }
+  minlevel: 5 | 7 | 15 | 40
+  itemlevel: 5 | 7 | 15 | 40
+  offertype: "eOfferNone" | "eOfferCeleste1"
+  techs: { tech: string }
+  sellable: "true" | "false" // FIXME should be `boolean`
+  tradeable: "true" | "false" // FIXME should be `boolean`
+  destroyable: "true" | "false" // FIXME should be `boolean`
+  civilization?: "Egypt" | "Greek" | "Celt" | "Persia" | "Norse" | "Babylonian"
   shortdescriptionid?: string // FIXME should be `number`
-  specialborder?: string // FIXME should be `boolean`
+  specialborder?: "true" // FIXME should be `boolean`
 }
 
 export interface Advisors {
