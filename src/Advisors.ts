645b23f49be5edc5321a523fb8e0364ed93fcad5
Abraham Schilling <AbrahamSchilling@gmail.com>
2018-12-23 22:13:57 +0100

    Update Advisor

--- src/Advisors.ts
+++ src/Advisors.ts
@@ -1,7 +1,40 @@
+/**
+ * ```
+ * {
+ *   name: "Sesostris_L_IV",
+ *   groupid: 121,
+ *   age: 3,
+ *   rarity: "legendary",
+ *   icon: "Celeste\\UserInterface\\Icons\\Advisors\\AdvisorSesostris5_db",
+ *   icontexturecoords: "0 0 1 1.0",
+ *   rollovertextid: "",
+ *   displaynameid: 150090,
+ *   displaydescriptionid: 150091,
+ *   sellcostoverride: {
+ *     capitalresource: {
+ *       quantity: 900,
+ *       type: "cCapResCoin",
+ *     },
+ *   },
+ *   minlevel: 40,
+ *   itemlevel: 40,
+ *   offertype: "eOfferCeleste1",
+ *   techs: {
+ *     tech: "Advisor_Sesostris_L",
+ *   },
+ *   sellable: "true",
+ *   tradeable: "true",
+ *   destroyable: "true",
+ *   civilization: "Egypt",
+ *   shortdescriptionid: "61615",
+ *   specialborder: "true",
+ * }
+ * ```
+ */
 export interface Advisor {
   name: string
   groupid: number
-  tech: number
+  age: number
   rarity: string
   icon: string
   icontexturecoords: string
@@ -10,7 +43,7 @@ export interface Advisor {
   displaydescriptionid: number
   sellcostoverride: {
     capitalresource: {
-      quantity: number,
+      quantity: number
       type: string,
     },
   }
@@ -19,11 +52,13 @@ export interface Advisor {
   offertype: string
   techs: {
     tech: string,
-  },
-  sellable: string
-  tradeable: string
-  destroyable: string
+  }
+  sellable: string // FIXME should be `boolean`
+  tradeable: string // FIXME should be `boolean`
+  destroyable: string // FIXME should be `boolean`
   civilization: string
+  shortdescriptionid: string // FIXME should be `number`
+  specialborder: string // FIXME should be `boolean`
 }
 
 export interface Advisors {
